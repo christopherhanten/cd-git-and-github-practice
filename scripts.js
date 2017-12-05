b8e077451dafe84dec231e58643a9116a4d42607
christopeher hanten <chanten@chanten-815.local>
2017-12-05 11:45:12 -0600

    intial commit

---
+++ scripts.js
@@ -0,0 +1,1 @@
+add: window.onload = function(){ var button = document.getElementById("button-yellow"); button.addEventListener("click", function(event){ prompt('Join Our Mailing List'); button.innerHTML = "Thanks for your email!" }); }
